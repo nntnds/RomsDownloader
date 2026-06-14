a18453d245d2e4c78693369eec4d56f156e3e0c2
nntnds <jasavchuk08@gmail.com>
2026-06-14 23:30:50 +0400

    small changes

--- app/src/main/java/com/nntndscvtcvt/romsdownloader/presentation/components/SectionHeader.kt
+++ app/src/main/java/com/nntndscvtcvt/romsdownloader/presentation/components/SectionHeader.kt
@@ -7,12 +7,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import com.nntndscvtcvt.romsdownloader.presentation.utils.Dimens
 
 @Composable
 fun SectionHeader(text: String) {
     Text(
         text = text,
+        color = MaterialTheme.colorScheme.primary,
+        style = MaterialTheme.typography.titleSmall,
         modifier = Modifier
             .fillMaxWidth()
             .background(MaterialTheme.colorScheme.surface)
--- app/src/main/java/com/nntndscvtcvt/romsdownloader/presentation/settings/components/ConsoleItem.kt
+++ app/src/main/java/com/nntndscvtcvt/romsdownloader/presentation/settings/components/ConsoleItem.kt
@@ -1,5 +1,8 @@
-package com.nntndscvtcvt.romsdownloader.presentation.settings.components.console
+package com.nntndscvtcvt.romsdownloader.presentation.settings.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
@@ -10,10 +13,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.nntndscvtcvt.romsdownloader.domain.model.DownloadGamesState
 import com.nntndscvtcvt.romsdownloader.presentation.settings.Console
 import com.nntndscvtcvt.romsdownloader.presentation.settings.components.common.IconBox
+import com.nntndscvtcvt.romsdownloader.presentation.settings.components.console.ConsoleDeleteDialog
+import com.nntndscvtcvt.romsdownloader.presentation.settings.components.console.ConsoleRedownloadDialog
+import com.nntndscvtcvt.romsdownloader.presentation.settings.components.console.ConsoleSupportingContent
+import com.nntndscvtcvt.romsdownloader.presentation.settings.components.console.ConsoleTrailingAction
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
@@ -61,7 +71,7 @@ fun ConsoleItem(
         },
         shapes = ListItemDefaults.segmentedShapes(index, totalCount),
         colors = ListItemDefaults.segmentedColors(
-            containerColor = if (state.isDownloading) MaterialTheme.colorScheme.surfaceContainerHigh
+            containerColor = if (state.isDownloading) MaterialTheme.colorScheme.surface
                 else MaterialTheme.colorScheme.surfaceContainer,
             supportingContentColor = MaterialTheme.colorScheme.onSurfaceVariant,
             trailingContentColor = MaterialTheme.colorScheme.primary
@@ -87,7 +97,8 @@ fun ConsoleItem(
                 text = item.consoleName,
                 style = MaterialTheme.typography.titleMedium,
                 maxLines = 1,
-                overflow = TextOverflow.Ellipsis
+                overflow = TextOverflow.Ellipsis,
+                color = MaterialTheme.colorScheme.onSurface
             )
         }
     )
